21f971aa0d28b7f5da94a784ff6c306de0a487e3
Bill Z (IWY_Page) <66582980+billzhao1030@users.noreply.github.com>
2022-09-12 18:15:27 +1000

    tut8

--- toolkit.py
+++ toolkit.py
@@ -17,7 +17,7 @@ import pickle
 
 # from gccNMF.gccNMFFunctions import *
 # from gccNMF.gccNMFPlotting import *
-#import tensorflow as tf
+import tensorflow as tf
 
 """
 Functions to work with image classification
@@ -122,7 +122,6 @@ def plot_kmeans(kmeans,x_):
     plt.show()
 """
 Class and functions for MNIST
-@SNTs
 """
 def _change_ont_hot_label(X):
     T = np.zeros((X.size, 10))
@@ -287,3 +286,73 @@ def ConvertToDense(X, y, shape):
     R = np.asarray(R)             # convert matrix object to ndarray object
     return R
   
+
+#######
+INPUT_SIGNAL_TYPES = [
+    "body_acc_x_",
+    "body_acc_y_",
+    "body_acc_z_",
+    "body_gyro_x_",
+    "body_gyro_y_",
+    "body_gyro_z_",
+    "total_acc_x_",
+    "total_acc_y_",
+    "total_acc_z_"
+]
+
+# Output classes to learn how to classify
+LABELS = [
+    "WALKING", 
+    "WALKING_UPSTAIRS", 
+    "WALKING_DOWNSTAIRS", 
+    "SITTING", 
+    "STANDING", 
+    "LAYING"
+]
+
+def load_har_data(path):
+    TRAIN = "train"
+    TEST = "test"
+    DATASET_PATH=path 
+
+    X_train_signals_paths = [os.path.join(DATASET_PATH,TRAIN,"Inertial Signals",signal+"train.txt") for signal in INPUT_SIGNAL_TYPES]
+    X_test_signals_paths = [os.path.join(DATASET_PATH,TEST,"Inertial Signals",signal+"test.txt") for signal in INPUT_SIGNAL_TYPES]
+    X_train = load_X(X_train_signals_paths)
+    X_test = load_X(X_test_signals_paths)
+
+    y_train_path = os.path.join(DATASET_PATH,TRAIN,"y_train.txt")
+    y_test_path = os.path.join(DATASET_PATH,TEST,"y_test.txt")
+
+    y_train = load_y(y_train_path)
+    y_test = load_y(y_test_path)
+
+    return X_train, y_train, X_test, y_test
+
+def load_X(X_signals_paths):
+    X_signals = []
+    
+    for signal_type_path in X_signals_paths:
+        file = open(signal_type_path, 'r')
+        # Read dataset from disk, dealing with text files' syntax
+        X_signals.append(
+            [np.array(serie, dtype=np.float32) for serie in [
+                row.replace('  ', ' ').strip().split(' ') for row in file
+            ]]
+        )
+        file.close()
+    
+    return np.transpose(np.array(X_signals), (1, 2, 0))
+
+def load_y(y_path):
+    file = open(y_path, 'r')
+    # Read dataset from disk, dealing with text file's syntax
+    y_ = np.array(
+        [elem for elem in [
+            row.replace('  ', ' ').strip().split(' ') for row in file
+        ]], 
+        dtype=np.int32
+    )
+    file.close()
+    
+    # Substract 1 to each output class for friendly 0-based indexing 
+    return y_ - 1
